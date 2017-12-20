2799e7e147e8cf8dea3377e8b27ba7b0754c4a9e
Elad <lesh366@gmail.com>
2017-12-20 16:04:18 +0200

    new setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+Installing python packages on client environment to use Etool
+"""
+import pip
+
+
+def install(package):
+    pip.main(['install', package])
+
+
+if __name__ == '__main__':
+    print("Stating setup for Etool")
+    install('boto3')
+    install('requests')
